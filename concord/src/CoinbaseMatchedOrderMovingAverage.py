28ae5f9316cab4fd32626261e7f36a886cf381b7
agallego <gallego.alexx@gmail.com>
2015-09-16 19:44:15 -0400

    Fix the order type to be 'match' from bitcoin

--- concord/src/CoinbaseMatchedOrderMovingAverage.py
+++ concord/src/CoinbaseMatchedOrderMovingAverage.py
@@ -25,26 +25,16 @@ class CoinbaseMatchedOrderMovingAverage(Computation):
 
     def init(self, ctx):
         self.concord_logger.info("Matched order moving avg init")
-        for h in self.concord_logger.handlers:
-            h.flush()
 
     def process_record(self, ctx, record):
-        #try:
         order = CoinbaseOrder(record.data)
-        self.concord_logger.info("Type: %s", order.type)
-        if order.type == 'open':
+        if order.type == 'match':
             sec = bottom_of_current_second()
             avg = self.cache[sec]
             avg.append(order.price)
             ctx.set_timer(str(sec), nseconds_from_now_in_millis(1))
 
-        # except Exception as e:
-        #     self.concord_logger.exception(e)
-        #     self.concord_logger.error(
-        #         "Could not process message %s ", record.data)
-
     def process_timer(self, ctx, key, time):
-        self.concord_logger.info("hello motto, timer %s", key)
         avg_time = int(key)
         avg = self.cache[avg_time]
         if avg is not None:
@@ -53,7 +43,7 @@ class CoinbaseMatchedOrderMovingAverage(Computation):
                 'avg': avg.avg()
             }
             self.producer.send_messages(b'match-avg', json.dumps(d))
-            self.concord_logger.error("Could not process timer %s ", key)
+            self.concord_logger.info("Saving to kafka for time: %s ", key)
 
     def metadata(self):
         return Metadata(name='match-orders-sec-avg', istreams=['btcusd'])
