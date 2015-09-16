b340d678d7ac475acabfff2d4e273ec14c0ad667
agallego <gallego.alexx@gmail.com>
2015-09-15 21:05:56 -0400

    Increasing the python version

--- concord/src/runner.bash
+++ concord/src/runner.bash
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -x
-tree
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    # if $SOURCE was a relative symlink, we need to resolve it
+    # relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 original=${PWD}
-echo "Directory: ${PWD}"
-if [[ -f $original/requirements.txt ]]; then
+requirements=$(find $DIR -name requirements.txt)
+
+echo "Mesos directory: ${PWD}"
+echo "Exec directory: ${DIR}"
+
+if [[ -f $requirements ]]; then
     # need to overcome pip 128 chars path - software... :'(
     dir=$(mktemp -d)
     cd $dir
     echo "Installing venv in $dir"
     virtualenv $dir/env
-    $dir/env/bin/pip install -r $original/requirements.txt
+    $dir/env/bin/pip install -r $requirements
     exec $dir/env/bin/python "$original/$@"
 else
     exec python "$original/$@"
