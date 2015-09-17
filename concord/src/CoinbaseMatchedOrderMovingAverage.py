e09bb4f3864da4155ec64e623c5146239bd3620e
agallego <gallego.alexx@gmail.com>
2015-09-17 11:13:19 -0400

    fixed the moving average to be closer to the real value

--- concord/src/CoinbaseMatchedOrderMovingAverage.py
+++ concord/src/CoinbaseMatchedOrderMovingAverage.py
@@ -2,24 +2,28 @@ import json
 import sys
 import time
 import concord
-import cachetools
-
 from kafka import SimpleProducer, KafkaClient
-
-#TODO(agallego), import subtypes->subtypes
+from collections import deque
 from concord.computation import (Computation, Metadata, serve_computation)
-from models.Avg import Avg
 from models.CoinbaseOrder import CoinbaseOrder
 from utils.time_utils import (time_millis, bottom_of_current_second,
                               nseconds_from_now_in_millis)
 
-# TODO(agallego): At the moment is NOT a moving average, it is the real average
-# the Avg.class holds an array and a counter of N to compute it
-#
+# stores 1k numbers as the avg. simpler, but def not correct
+class MovingAvg(deque):
+    def __init__(self, size=1000):
+        super(MovingAvg, self).__init__(maxlen=size)
+
+    @property
+    def average(self):
+        if len(self) <= 0: return 0
+        return sum(self)/len(self)
+
+
+# TODO(agallego): use pandas / numpy
 class CoinbaseMatchedOrderMovingAverage(Computation):
     def __init__(self):
-        from cachetools import TTLCache
-        self.cache = TTLCache(10, 100, missing=lambda x: Avg());
+        self.moving_average = MovingAvg()
         self.kafka = KafkaClient('localhost:9092')
         self.producer = SimpleProducer(self.kafka, async=True)
 
@@ -32,21 +36,18 @@ class CoinbaseMatchedOrderMovingAverage(Computation):
             self.concord_logger.info("Found matched order at price: %s",
                                      str(order.price))
             sec = bottom_of_current_second()
-            avg = self.cache[sec]
-            avg.append(order.price)
+            self.moving_average.append(order.price)
             ctx.set_timer(str(sec), nseconds_from_now_in_millis(1))
 
     def process_timer(self, ctx, key, time):
-        self.concord_logger.info("Emmiting avg for second: %s", key)
         avg_time = int(key)
-        avg = self.cache[avg_time]
-        if avg is not None:
-            d = {
-                'time': avg_time * 1000,
-                'avg': avg.avg()
-            }
-            self.producer.send_messages(b'match-avg', json.dumps(d))
-            self.concord_logger.info("Saving to kafka for time: %s ", key)
+        d = {
+            'time': avg_time * 1000,
+            'avg': self.moving_average.average
+        }
+        self.producer.send_messages(b'match-avg', json.dumps(d))
+        self.concord_logger.info("Saving to kafka for time: %s, avg price: ",
+                                 key, self.moving_average.average)
 
     def metadata(self):
         return Metadata(name='match-orders-sec-avg', istreams=['btcusd'])
